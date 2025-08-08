a66815a5f7e38cf36cbabf41a1626ad75a420723
gabriel-ian <gabrielcenci43@gmail.com>
2025-08-07 21:38:36 -0300

    BoloVulcao

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+    public static void main(String[] args) {
+        /*
+        Desenvolver uma nova aplicação JAVA onde grava em uma variavel
+        cada ingrediente da reiceita de um bolo vulcão adormecido,
+        sem recheio e cobertura, e ao final exiba a receita completa,
+        com o modo de preparo.
+         */
+
+
+        String manteiga = " 100g de manteiga";
+        String ovo = " 3 ovos";
+        String acucar = " 1 e meia xícara(chá) de açúcar";
+        String chocolate = " 1 xícara (chá) de Chocolate em Pó";
+        String leite = " 1 xícara (chá) de Leite";
+        String farinha = " 2 xícaras (chá) de farinha de trigo";
+        String fermento = " 1 colher (sopa) de fermento em pó";
+        String bicarbonato = " meia colher (chá) de bicarbonato de sódio";
+        System.out.println("\nIngredientes para fazer um \uD83C\uDF82BOLO VALCÃO\uD83C\uDF0B");
+        System.out.println(manteiga + "\n" + ovo + "\n" + acucar + "\n" + chocolate + "\n" + leite + "\n" + farinha + "\n" + fermento + "\n" + bicarbonato);
+
+        System.out.println("\nUtensílios necessários" + "\n Panela" + "\n Forno" + "\n Batedeira" + "\n Forma de Furo do meio");
+
+        System.out.println("\nModo de preparo\n" + "\nEm uma batedeira, bata a manteiga, os ovos, o açúcar e o Chocolate em Pó DOIS FRADES até ficar homogêneo.\n" +
+                "Desligue a batedeira, junte o Leite NINHO, a farinha, o fermento e o bicarbonato e misture bem.\n" +
+                "Coloque a massa do bolo vulcão em uma forma redonda de furo central (23 cm de diâmetro)\nuntada com manteiga e polvilhada com farinha de trigo,\ne leve ao forno médio-alto (200°C), preaquecido, por cerca de 40 minutos.\n" +
+                "Espere amornar e desenforme.");
+
+
+
+
+
+
+
+    }
+}
